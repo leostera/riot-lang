8acd79de0f99abcc6df478c8d17cae84fb189cdd
Leandro Ostera <leandro@abstractmachines.dev>
2025-09-30 20:33:51 +0200

    fix(tusk): convert Module_name.t to string in module_registry
    - Module.module_name returns Module_name.t
    - Convert to string before using as hashtbl key

--- packages/tusk/src/core/module_registry.ml
+++ packages/tusk/src/core/module_registry.ml
@@ -1,27 +1,29 @@
 (** Module registry for tracking module graph nodes by name *)
 
 open Std
+open Std.Graph
 open Model
 
 type t = {
-  modules : (Graph.SimpleGraph.Node_id.t, Module.t) Hashtbl.t;
-  intf_by_name : (string, Graph.SimpleGraph.Node_id.t) Hashtbl.t;
-  impl_by_name : (string, Graph.SimpleGraph.Node_id.t) Hashtbl.t;
+  modules : (SimpleGraph.Node_id.t, Module.t) Hashtbl.t;
+  intf_by_name : (string, SimpleGraph.Node_id.t) Hashtbl.t;
+  impl_by_name : (string, SimpleGraph.Node_id.t) Hashtbl.t;
 }
 
-let create () = {
-  modules = Hashtbl.create 16;
-  intf_by_name = Hashtbl.create 16;
-  impl_by_name = Hashtbl.create 16;
-}
+let create () =
+  {
+    modules = Hashtbl.create 16;
+    intf_by_name = Hashtbl.create 16;
+    impl_by_name = Hashtbl.create 16;
+  }
 
 let register t mod_ node_id =
   Hashtbl.add t.modules node_id mod_;
-  let mod_name = Module.name mod_ |> Module_name.to_string in
+  let mod_name = Module.module_name mod_ |> Module_name.to_string in
   let table =
     match Module.kind mod_ with
-    | Module.Implementation -> t.impl_by_name
-    | Module.Interface -> t.intf_by_name
+    | `implementation -> t.impl_by_name
+    | `interface -> t.intf_by_name
   in
   Hashtbl.add table mod_name node_id
 
@@ -35,6 +37,4 @@ let get_by_name t name =
   (match Hashtbl.find_opt t.impl_by_name name with
   | Some node -> nodes := node :: !nodes
   | None -> ());
-  match !nodes with
-  | [] -> raise Not_found
-  | nodes -> nodes
+  match !nodes with [] -> raise Not_found | nodes -> nodes
