d490ac5aaf33d82365feb14cd2e9201c0aefc1e8
Leandro Ostera <leandro@abstractmachines.dev>
2025-10-03 16:12:24 +0200

    refactor(tusk): use local open for cleaner CLI definition
    Use 'let open ArgParser in' to avoid repeating ArgParser.* everywhere.
    Makes the CLI definition much more readable and concise.

--- packages/tusk/src/cli/cli2.ml
+++ packages/tusk/src/cli/cli2.ml
@@ -23,52 +23,46 @@ let usage_msg =
 |}
 
 let cli =
-  ArgParser.command "tusk" ~version:(Some "0.1.0")
+  let open ArgParser in
+  command "tusk" ~version:(Some "0.1.0")
     ~about:(Some "OCaml build system and package manager") ()
-  |> ArgParser.arg
+  |> arg
        Arg.(
          flag "verbose" |> short 'v' |> long "verbose"
          |> help "Enable verbose output" |> count)
-  |> ArgParser.subcommand
-       (ArgParser.command "build" ~about:(Some "Build packages") ()
-       |> ArgParser.arg
+  |> subcommand
+       (command "build" ~about:(Some "Build packages") ()
+       |> arg
             Arg.(
               option "package" |> short 'p' |> long "package"
               |> help "Build only the specified package"))
-  |> ArgParser.subcommand
-       (ArgParser.command "run" ~about:(Some "Run a binary") ()
-       |> ArgParser.arg
+  |> subcommand
+       (command "run" ~about:(Some "Run a binary") ()
+       |> arg
             Arg.(
               option "binary" |> short 'b' |> long "binary"
               |> help "Specify which binary to run"))
-  |> ArgParser.subcommand
-       (ArgParser.command "clean" ~about:(Some "Clean build artifacts") ())
-  |> ArgParser.subcommand
-       (ArgParser.command "new" ~about:(Some "Create a new package") ()
-       |> ArgParser.arg Arg.(positional "path" |> help "Path for new package"))
-  |> ArgParser.subcommand
-       (ArgParser.command "install"
-          ~about:(Some "Install a binary to ~/.tusk/bin") ()
-       |> ArgParser.arg
+  |> subcommand (command "clean" ~about:(Some "Clean build artifacts") ())
+  |> subcommand
+       (command "new" ~about:(Some "Create a new package") ()
+       |> arg Arg.(positional "path" |> help "Path for new package"))
+  |> subcommand
+       (command "install" ~about:(Some "Install a binary to ~/.tusk/bin") ()
+       |> arg
             Arg.(
               option "binary" |> short 'b' |> long "binary"
               |> help "Binary to install"))
-  |> ArgParser.subcommand
-       (ArgParser.command "server"
-          ~about:(Some "Start or manage the tusk server") ()
-       |> ArgParser.arg
+  |> subcommand
+       (command "server" ~about:(Some "Start or manage the tusk server") ()
+       |> arg
             Arg.(
               option "action" |> help "Action: start, stop, kill, or status"
               |> possible_values [ "start"; "stop"; "kill"; "status" ]))
-  |> ArgParser.subcommand
-       (ArgParser.command "rpc" ~about:(Some "Send RPC command to server") ())
-  |> ArgParser.subcommand
-       (ArgParser.command "mcp"
-          ~about:(Some "Start Model Context Protocol server") ())
-  |> ArgParser.subcommand
-       (ArgParser.command "version" ~about:(Some "Show tusk version") ())
-  |> ArgParser.subcommand
-       (ArgParser.command "help" ~about:(Some "Show help message") ())
+  |> subcommand (command "rpc" ~about:(Some "Send RPC command to server") ())
+  |> subcommand
+       (command "mcp" ~about:(Some "Start Model Context Protocol server") ())
+  |> subcommand (command "version" ~about:(Some "Show tusk version") ())
+  |> subcommand (command "help" ~about:(Some "Show help message") ())
 
 let main ~args =
   let argc = List.length args in
