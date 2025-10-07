d17dbd9aa51d8c870fea5fad3e62146f5c14151a
Leandro Ostera <leandro@abstractmachines.dev>
2025-10-08 00:50:02 +0200

    feat(syn): add try-catch expression support and fix match first-case pipe handling
    - Implement parse_try_expr for try-with-exception handling
    - Refactor match case parsing to support optional leading pipe
    - Add parse_match_case_no_pipe for first match/try case without |
    - Add parse_match_case_body to share logic between piped and non-piped cases
    - Fix: match/try first case no longer requires leading | (valid OCaml)
    - Regenerate all match and try test expected files
    - Tests passing: 306/500 (61.2%)

--- packages/sandwormsay/src/main.ml
+++ packages/sandwormsay/src/main.ml
@@ -1,62 +1,55 @@
 open Std
 
-let sandworm =
-  {|
-    __________________
-   /                  \
-  /  O    O    O    O  \
- |  ____________________  |
- | |                    | |
- | |  THE SPICE MUST   | |
- | |      FLOW         | |
- | |____________________| |
-  \________________________/
-       |||||||||||||||
-      |||||||||||||||
-     |||||||||||||||
-    |||||||||||||||
-   |||||||||||||||
-  |||||||||||||||
- |||||||||||||||
-|||||||||||||||
- |||||||||||||||
-  |||||||||||||||
-   |||||||||||||||
-    |||||||||||||||
-     |||||||||||||||
-      ~~~~~~~~~~~~~
+let sandworm = {|
+                                    ___
+                                 .-'   `'.
+                                /         \
+                                |         ;
+                                |         |           ___.--,
+                       _.._     |0) ~ (0) |    _.---'`__.-( (_.
+                __.--'`_.. '.__.\    '--. \_.-' ,.--'`     `""`
+               ( ,.--'`   ',__ /./;   ;, '.__.'`    __
+               _`) )  .---.__.' / |   |\   \__..--""  """--.,_
+              `---' .'.''-._.-'`_./  /\ '.  \ _.-~~~````~~~-._`-.__.'
+                    | |  .' _.-' |  |  \  \  '.               `~---`
+                     \ \/ .'     \  \   '. '-._)
+                      \/ /        \  \    `=.__`~-.
+                      / /\         `) )    / / `"".`\
+                ,. ,.-'.'  \        / /    ( (     / /
+                 `'`-'      \_    .(` )     \ \_.-' /
+                             `---` \ \/       `-'  /
+                                    ) )            (
+                                   (_/             `
 |}
 
-let wrap_message msg =
+let print_message msg =
   let lines = String.split_on_char '\n' msg in
-  let max_len =
-    List.fold_left (fun acc line -> max acc (String.length line)) 0 lines
-  in
-  let border_top = " " ^ String.make (max_len + 2) '_' in
-  let border_bottom = " " ^ String.make (max_len + 2) '-' in
+  let max_len = List.fold_left (fun acc line ->
+    Int.max acc (String.length line)
+  ) 0 lines in
 
-  let format_line line =
-    let padding = max_len - String.length line in
-    "| " ^ line ^ String.make padding ' ' ^ " |"
-  in
+  (* Top border *)
+  Format.printf " %s\n" (String.make (max_len + 2) '_');
 
-  let formatted_lines = List.map format_line lines in
-  String.concat "\n" ((border_top :: formatted_lines) @ [ border_bottom ])
+  (* Message with side borders *)
+  List.iter (fun line ->
+    let padding = String.make (max_len - String.length line) ' ' in
+    Format.printf "< %s%s >\n" line padding
+  ) lines;
 
-let sandworm_with_message msg =
-  let bubble = wrap_message msg in
-  let worm_lines = String.split_on_char '\n' sandworm in
+  (* Bottom border *)
+  Format.printf " %s\n" (String.make (max_len + 2) '-');
+  ()
 
-  bubble ^ "\n" ^ String.concat "\n" ("      \\" :: List.tl worm_lines)
-
-let main () =
+let () =
   let args = Sys.argv in
   let message =
     if Array.length args > 1 then
       String.concat " " (List.tl (Array.to_list args))
-    else "Bless the Maker and His water. Bless the coming and going of Him."
+    else
+      "Bless the Maker and His water. Bless the coming and going of Him."
   in
 
-  print_endline (sandworm_with_message message)
-
-let () = main ()
+  print_message message;
+  Format.printf "%s" sandworm;
+  Format.printf "\n"
