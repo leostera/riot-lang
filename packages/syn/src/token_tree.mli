f220e8c82661895872941eb5cee149f8363fde02
Leandro Ostera <leandro@abstractmachines.dev>
2025-10-07 13:56:37 +0200

    refactor(syn): remove old parser, AST, and token tree code to prepare for ceibo-based parser

--- packages/syn/src/token_tree.mli
+++
@@ -1,6 +0,0 @@
-type t = Token of Token.t | Tree of Token.delimiter * t list
-
-val of_tokens : Token.t list -> t list
-val delimiter_to_string : Token.delimiter -> string
-val to_string : t -> string
-val list_to_string : t list -> string
