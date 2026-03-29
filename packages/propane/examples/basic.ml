df97646de8e2e2edcf915126c8c5f15ec690e617
Leandro Ostera <leandro@abstractmachines.dev>
2026-03-29 11:57:04 +0200

    style: commit vetted propane reformat batch

--- packages/propane/examples/basic.ml
+++ packages/propane/examples/basic.ml
@@ -2,44 +2,29 @@ open Std
 open Propane
 
 (* Simple property: reversing a list twice gives the original list *)
-let list_rev_prop = 
-  property "list reverse is involutive" 
-    Arbitrary.(list int) 
-    (fun lst -> List.rev (List.rev lst) = lst)
+let list_rev_prop = property "list reverse is involutive" Arbitrary.(list int) (fun lst -> List.rev (List.rev
+lst)
+= lst)
 
 (* Property with assumptions *)
-let division_prop =
-  property "division and modulo relation"
-    Arbitrary.(pair int int)
-    (fun (a, b) ->
-      assume (b != 0);
-      (a / b) * b + (a mod b) = a)
+let division_prop = property "division and modulo relation" Arbitrary.(pair int int)
+  (fun (a, b) ->
+    assume (b != 0);
+    (a / b) * b + (a mod b) = a)
 
 (* Vector property *)
-let vector_length_prop =
-  property "vector length after push"
-    Arbitrary.(pair int (vector int))
-    (fun (x, vec) ->
-      let original_len = Collections.Vector.len vec in
-      Collections.Vector.push vec x;
-      Collections.Vector.len vec = original_len + 1)
+let vector_length_prop = property "vector length after push" Arbitrary.(pair int (vector int))
+  (fun (x, vec) ->
+    let original_len = Collections.Vector.len vec in
+    Collections.Vector.push vec x;
+    Collections.Vector.len vec = original_len + 1)
 
 (* String property *)
-let string_concat_prop =
-  property "string concatenation length"
-    Arbitrary.(pair string string)
-    (fun (s1, s2) ->
-      String.length (s1 ^ s2) = String.length s1 + String.length s2)
+let string_concat_prop = property "string concatenation length" Arbitrary.(pair string string) (fun (s1, s2) -> String.length (s1
+^ s2)
+= String.length s1 + String.length s2)
 
 (* All tests - ready to use with Test.Cli.main! *)
-let tests = [
-  list_rev_prop;
-  division_prop;
-  vector_length_prop;
-  string_concat_prop;
-]
+let tests = [ list_rev_prop; division_prop; vector_length_prop; string_concat_prop ]
 
-let () =
-  Miniriot.run 
-    ~main:(fun ~args -> Test.Cli.main ~name:"propane-basic-examples" ~tests ~args) 
-    ~args:Env.args ()
+let () = Miniriot.run ~main:(fun ~args -> Test.Cli.main ~name:"propane-basic-examples" ~tests ~args) ~args:Env.args ()
--- packages/propane/src/Propane.ml
+++ packages/propane/src/Propane.ml
@@ -1,7 +1,5 @@
 open Std
-
 (** Main module for Propane property testing library *)
-
 module Generator = Generator
 module Shrinker = Shrinker
 module Printer = Printer
@@ -10,8 +8,13 @@ module Property = Property
 
 (* Convenience re-exports *)
 let property = Property.property
+
 let for_all = Property.for_all
+
 let implies = Property.implies
+
 let assume = Property.assume
+
 let assume_fail = Property.assume_fail
+
 let fail = Property.fail
