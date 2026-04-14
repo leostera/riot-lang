75c8bceec66fd20f66b926c9ef9bd397bec28486
Leandro Ostera <leandro@abstractmachines.dev>
2026-04-14 23:43:57 +0200

    chore(actors): remove package as it was absorbed into std

--- packages/actors/fix/riot_fix_rules.ml
+++
@@ -1,5 +0,0 @@
-let name = "actors"
-
-let rules = fun () -> [ Actors_loop_yield.rule () ]
-
-let explanations = fun () -> Actors_loop_yield.explanations ()
--- packages/actors/src/actors.mli
+++
@@ -1,6 +0,0 @@
-(** Compatibility facade for the Riot actor runtime.
-
-    The runtime implementation now lives in `Std.Runtime`. This package keeps
-    the historical `Actors` entrypoint available while the rest of the repo
-    migrates. *)
-include module type of Std.Runtime
