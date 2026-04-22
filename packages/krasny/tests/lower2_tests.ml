cc582820fcff88ab6fe16e8c12a5203de676d388
Leandro Ostera <leandro@abstractmachines.dev>
2026-04-23 01:51:31 +0200

    test(krasny): cover record type docs in lower2

--- packages/krasny/tests/lower2_tests.ml
+++ packages/krasny/tests/lower2_tests.ml
@@ -103,6 +103,7 @@ let assert_lower2_existing_fixture_subset = fun () ->
     Path.v "packages/krasny/tests/fixtures/0732_signature_section_between_value_docstrings.mli";
     Path.v "packages/krasny/tests/fixtures/0733_signature_value_docstring_then_section_then_type_docstrings.mli";
     Path.v "packages/krasny/tests/fixtures/0734_signature_type_trailing_doc_then_heading.mli";
+    Path.v "packages/krasny/tests/fixtures/0735_signature_record_trailing_doc_then_heading.mli";
   ]
   in
   let rec loop = function
