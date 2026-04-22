5a2f47c62fe5e53a622c2337d59fa3f6747f2b40
Leandro Ostera <leandro@abstractmachines.dev>
2026-04-23 01:53:11 +0200

    test(krasny): cover signature sum type docs in lower2

--- packages/krasny/tests/lower2_tests.ml
+++ packages/krasny/tests/lower2_tests.ml
@@ -105,6 +105,7 @@ let assert_lower2_existing_fixture_subset = fun () ->
     Path.v "packages/krasny/tests/fixtures/0734_signature_type_trailing_doc_then_heading.mli";
     Path.v "packages/krasny/tests/fixtures/0735_signature_record_trailing_doc_then_heading.mli";
     Path.v "packages/krasny/tests/fixtures/0736_signature_module_overview_before_open_then_type_doc.mli";
+    Path.v "packages/krasny/tests/fixtures/0737_signature_sum_type_trailing_doc_then_next_type_leading_doc.mli";
   ]
   in
   let rec loop = function
