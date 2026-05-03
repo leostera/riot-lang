67b9228f6eb09c1a91e4d998948ed22afb363ad7
Leandro Ostera <leandro@abstractmachines.dev>
2026-05-03 13:21:07 +0200

    refactor(blink): remove endpoint class policy

--- packages/blink/src/client/request.ml
+++ packages/blink/src/client/request.ml
@@ -7,37 +7,21 @@ type method_ =
   | Patch
   | Delete
 
-type endpoint_class =
-  | PublicRead
-  | PrivateRead
-  | TradingWrite
-  | ExternalRead
-
 type t = {
   method_: method_;
   url: string;
   headers: (string * string) list;
   body: string option;
-  endpoint_class: endpoint_class;
   auth_required: bool;
   deadline: Time.Duration.t option;
 }
 
-let make = fun
-  ?(headers = [])
-  ?body
-  ?(endpoint_class = PublicRead)
-  ?(auth_required = false)
-  ?deadline
-  ~method_
-  ~url
-  () ->
+let make = fun ?(headers = []) ?body ?(auth_required = false) ?deadline ~method_ ~url () ->
   {
     method_;
     url;
     headers;
     body;
-    endpoint_class;
     auth_required;
     deadline;
   }
@@ -49,12 +33,5 @@ let method_to_string = fun value ->
   | Put -> "PUT"
   | Patch -> "PATCH"
   | Delete -> "DELETE"
-
-let endpoint_class_to_string = fun value ->
-  match value with
-  | PublicRead -> "public_read"
-  | PrivateRead -> "private_read"
-  | TradingWrite -> "trading_write"
-  | ExternalRead -> "external_read"
 
 let describe = fun request -> method_to_string request.method_ ^ " " ^ request.url
--- packages/blink/src/client/request.mli
+++ packages/blink/src/client/request.mli
@@ -6,17 +6,11 @@ type method_ =
   | Put
   | Patch
   | Delete
-type endpoint_class =
-  | PublicRead
-  | PrivateRead
-  | TradingWrite
-  | ExternalRead
 type t = {
   method_: method_;
   url: string;
   headers: (string * string) list;
   body: string option;
-  endpoint_class: endpoint_class;
   auth_required: bool;
   deadline: Time.Duration.t option;
 }
@@ -24,7 +18,6 @@ type t = {
 val make:
   ?headers:(string * string) list ->
   ?body:string ->
-  ?endpoint_class:endpoint_class ->
   ?auth_required:bool ->
   ?deadline:Time.Duration.t ->
   method_:method_ ->
@@ -33,7 +26,5 @@ val make:
   t
 
 val method_to_string: method_ -> string
-
-val endpoint_class_to_string: endpoint_class -> string
 
 val describe: t -> string
