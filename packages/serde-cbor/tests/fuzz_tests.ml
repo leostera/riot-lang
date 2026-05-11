306684c00e46038706b45c21fd82bae9330d8343
Leandro Ostera <leandro@abstractmachines.dev>
2026-05-11 18:12:39 +0200

    test(serde-cbor): add fuzz coverage

---
+++ packages/serde-cbor/tests/fuzz_tests.ml
@@ -0,0 +1,86 @@
+open Std
+
+module Test = Std.Test
+module De = Serde.De
+
+let accept_rejection = fun fn ->
+  try
+    fn ()
+    |> ignore
+  with
+  | _ -> ()
+
+type sample_field =
+  | Field_name
+  | Field_count
+  | Field_active
+  | Field_tags
+  | Field_nickname
+
+let sample_fields =
+  De.fields
+    [
+      De.field "name" Field_name;
+      De.field "count" Field_count;
+      De.field "active" Field_active;
+      De.field "tags" Field_tags;
+      De.field "nickname" Field_nickname;
+    ]
+
+let sample_decode =
+  De.record
+    ~fields:sample_fields
+    ~init:()
+    ~step:(fun reader () field ->
+      (
+        match field with
+        | Some Field_name ->
+            De.read reader De.string
+            |> ignore
+        | Some Field_count ->
+            De.read reader De.int
+            |> ignore
+        | Some Field_active ->
+            De.read reader De.bool
+            |> ignore
+        | Some Field_tags ->
+            De.read reader (De.list De.string)
+            |> ignore
+        | Some Field_nickname ->
+            De.read reader (De.option De.string)
+            |> ignore
+        | None ->
+            De.read reader De.skip_any
+            |> ignore
+      );
+      ())
+    ~finish:(fun () -> ())
+
+let mutator =
+  Test.Fuzz.Mutator.(bytes
+  |> with_max_len 2_048
+  |> with_dictionary [ ""; "\xf6"; "\xf5"; "\x00"; "\x63riot"; "\xa0"; "\x80"; ])
+
+let test_decode_fuzz = fun _ctx input ->
+  accept_rejection (fun () -> Serde_cbor.from_string De.skip_any input);
+  accept_rejection (fun () -> Serde_cbor.from_string De.bool input);
+  accept_rejection (fun () -> Serde_cbor.from_string De.string input);
+  accept_rejection (fun () -> Serde_cbor.from_string De.int input);
+  accept_rejection (fun () -> Serde_cbor.from_string (De.list De.string) input);
+  accept_rejection (fun () -> Serde_cbor.from_string (De.dict De.skip_any) input);
+  accept_rejection (fun () -> Serde_cbor.from_string sample_decode input);
+  accept_rejection (fun () -> Serde_cbor.from_reader sample_decode (IO.Reader.from_string input));
+  Ok ()
+
+let tests =
+  Test.[
+    fuzz
+      "serde-cbor decoders accept arbitrary bytes"
+      ~seeds:[ ""; "\xf6"; "\xa0"; "\xa1\x64name\x64riot"; ]
+      ~mutator
+      test_decode_fuzz;
+  ]
+
+let main ~args = Test.Cli.main ~name:"serde_cbor_fuzz_tests" ~tests ~args ()
+
+let () = Std.Runtime.run ~main ~args:Std.Env.args ()
