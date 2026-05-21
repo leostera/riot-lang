dffbb8aa22dc5bf262493e95ed87d49a024d3540
Leandro Ostera <leandro@abstractmachines.dev>
2026-05-22 01:18:07 +0200

    test(stage0): use mutual recursion in rename fixture

--- compiler/fixtures/programs/basic/compiler_like_rename_expr.ml
+++ compiler/fixtures/programs/basic/compiler_like_rename_expr.ml
@@ -17,26 +17,39 @@ fn rename_name(table: List<rename>, name: String) -> String {
   }
 }
 
+fn rename_args(table: List<rename>, args: List<expr>) -> List<expr> {
+  match args {
+    [] -> [],
+    [arg, ..rest] -> [rename_expr(table, arg), ..rename_args(table, rest)]
+  }
+}
+
 fn rename_expr(table: List<rename>, expr: expr) -> expr {
   match expr {
     Name(name) -> Name(rename_name(table, name)),
-    Call(callee, args) -> Call(rename_name(table, callee), args),
+    Call(callee, args) -> Call(rename_name(table, callee), rename_args(table, args)),
     Let(name, value, body) -> {
       let renamed = rename_name(table, name);
-      Let(renamed, rename_expr(table, value), rename_expr([rename { from: name, to: renamed }], body))
+      Let(renamed, rename_expr(table, value), rename_expr([rename { from: name, to: renamed }, ..table], body))
     }
+  }
+}
+
+fn render_args(args: List<expr>) -> String {
+  match args {
+    [] -> "",
+    [arg, ..rest] ->
+      match rest {
+        [] -> render(arg),
+        _ -> string_concat(render(arg), string_concat(", ", render_args(rest)))
+      }
   }
 }
 
 fn render(expr: expr) -> String {
   match expr {
     Name(name) -> name,
-    Call(callee, args) ->
-      match args {
-        [] -> string_concat(callee, "()"),
-        [Name(name), .._] -> string_concat(callee, string_concat("(", string_concat(name, ")"))),
-        _ -> string_concat(callee, "(?)")
-      },
+    Call(callee, args) -> string_concat(callee, string_concat("(", string_concat(render_args(args), ")"))),
     Let(name, value, body) -> string_concat("let ", string_concat(name, string_concat(" = ", string_concat(render(value), string_concat("; ", render(body))))))
   }
 }
