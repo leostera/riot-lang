5c4784e72f60838fc1e49920b546b1951bbf00fa
Leandro Ostera <leandro@abstractmachines.dev>
2026-05-22 00:22:51 +0200

    feat(riotc): add stage0-built hello entrypoint

--- compiler/riotc/src/main.ml
+++ compiler/riotc/src/main.ml
@@ -1,7 +1,4 @@
-open Std
-
-let main ~args:_ =
-  println "Hello, World!";
-  Ok ()
-
-let () = Runtime.run ~main ~args:Env.args ()
+fn main(args: List<String>) -> i32 {
+  println("riotc bootstrap");
+  0
+}
